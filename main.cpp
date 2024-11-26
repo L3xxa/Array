8a3e99b5dc300a2ccb2d6126e21ffbec45b66783
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-11-26 16:46:14 +0200

    complete

--- main.cpp
+++ main.cpp
@@ -2,13 +2,19 @@
 using namespace std;
 
 int main() {
+    int profit;
+    int sum_profit [6];
 
-    int arr[4];
+    for (int i = 0; i < 6; i++) {
+        cout << "Enter the profit for the month: ";
+        cin >> profit;
+        sum_profit[i] = profit;
+    }
 
-    arr[0] = 5;
-    arr[1] = 10;
+    cout << "Income for 6 months ="
+    << sum_profit[0] + sum_profit[1]
+    + sum_profit[2] + sum_profit[3]
+    + sum_profit[4] + sum_profit[5] << endl;
 
-    cout << arr[0] << endl;
-
-
+    return 0;
 }
