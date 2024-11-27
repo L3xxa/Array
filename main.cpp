200f8f1b3ff633944ebeb63a32aa4c31ae7a1d14
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-11-27 12:25:17 +0200

    complete

--- main.cpp
+++ main.cpp
@@ -1,14 +1,41 @@
 #include <iostream>
+#include <random>
 using namespace std;
 
 int main() {
+    constexpr int SIZE = 10;
+    int masive [SIZE];
 
-    int arr[4];
-
-    arr[0] = 5;
-    arr[1] = 10;
-
-    cout << arr[0] << endl;
+    random_device rd;
+    mt19937 mersenne(rd());
+    uniform_int_distribution<> uid(-100, 100);
 
 
+    for (int i = 0; i < SIZE; ++i) {
+        masive[i] = uid(mersenne);
+        cout << masive[i] << " ";
+    }
+    cout << endl;
+
+
+    int max = 0;
+    for (int i = 1; i < SIZE; i++ ) {
+        if (masive[i] > max) {
+            max = masive[i];
+        }
+
+    }
+    cout << "Max element: " << max << endl;
+
+    int min = 0;
+    for (int i = 1; i < SIZE; i++ ) {
+        if (masive[i] < min) {
+            min = masive[i];
+        }
+
+    }
+    cout << "Min element: " << min << endl;
+
+
+    return 0;
 }
